c77c1a8a34962606471ac72e57d5b0b1b59879dc
Geksus <alexgergel@gmail.com>
2024-06-20 16:45:17 +0300

    Client side completed

---
+++ client/src/app/app.config.ts
@@ -0,0 +1,19 @@
+import { provideHttpClient, withFetch } from "@angular/common/http";
+
+import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { provideRouter } from "@angular/router";
+
+import { routes } from "./app.routes";
+import { provideClientHydration } from "@angular/platform-browser";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(), provideAnimationsAsync(),
+  ],
+};
